661ff9df6cad8900d21fa96352801630a0ddf3e0
Rogerio da Silva <rogerleonino@gmail.com>
2023-11-17 13:51:48 -0300

    Removido libs não utilzadas da viw Cadastrar_chamada, do arquivo.py j_chamarPaciente

--- Medicos/views/j_chamarPaciente.py
+++ Medicos/views/j_chamarPaciente.py
@@ -18,41 +18,13 @@ from reportlab.lib.units import inch
 from reportlab.pdfgen import canvas
 from reportlab.platypus import Paragraph
 
-
-from django.shortcuts import render, HttpResponse
-from io import BytesIO
-from reportlab.lib.units import mm
-from reportlab.lib.colors import black
-from Triagem.models import triagem
-
-
 from Atendimento.models import *
-#from Medicos.forms import Form_medico_atendimento
-from Medicos.models import CustomUser
-from Triagem.models import triagem
-
-import chardet
-from Medicos.models import cid_10
-
-
-from Medicos.models import CustomUser
-from django import forms
-from django.contrib.auth.mixins import LoginRequiredMixin
-
-# views.py
-from django.contrib.auth.hashers import make_password
-from Medicos.models import User
-
 from django.http import JsonResponse
-from Medicos.models import Chamar_P_para_atendimento, CadastroSala, Salas_Atendimento
+from Medicos.models import Chamar_P_para_atendimento
 
-
-# ---------------------------------------------- END  ----------------------------------------------
 def cadastrar_chamada(request):
     if request.method == 'POST':
         nome_paciente = request.POST.get('nome')
-        print(f'Este é o nome do paciente: {nome_paciente}')
-        print(f'Este é o ID do usuário: {request.user.id}')
         chamada = Chamar_P_para_atendimento(nome_paciente=nome_paciente, request=request)
         chamada.save()
         return JsonResponse({'success': True})
