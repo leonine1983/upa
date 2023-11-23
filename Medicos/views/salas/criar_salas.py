e237c3dfa5913cf971fda21c51c976d3a9c2447a
Rogerio da Silva <rogerleonino@gmail.com>
2023-11-23 14:20:58 -0300

    Adicionado paginator na tabela de salas em cadastro de salas

--- Medicos/views/salas/criar_salas.py
+++ Medicos/views/salas/criar_salas.py
@@ -6,13 +6,26 @@ from Medicos.models import  CadastroSala
 from Medicos.views.salas.salas_form import Salas_form
 
 
+
+
+from django.core.paginator import Paginator
+from django.views.generic import CreateView
+from Medicos.models import CadastroSala
+from Medicos.views.salas.salas_form import Salas_form
+
 class Cadastra_Sala_view(LoginRequiredMixin, CreateView):
     model = CadastroSala
     template_name = 'Medicos/salas/salas.html'
     form_class = Salas_form
     success_url = reverse_lazy('Medicos:salas')
+    paginate_by = 10 # Defina o número de itens por página
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["conteudo"] = "cadastraSala"
+        # Obtenha todos os objetos da classe CadastroSala
+        # e divida-os em páginas usando a classe Paginator
+        paginator = Paginator(CadastroSala.objects.all(), self.paginate_by)
+        page_number = self.request.GET.get('page')  # Obtenha o número da página da URL
+        context["object_list"] = paginator.get_page(page_number)
         return context
