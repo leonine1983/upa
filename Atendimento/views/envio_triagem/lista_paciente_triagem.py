8f6383e6793b82e42d563a3bfad6d53af761dce0
RubemJanoni <rubemjanoni@gmail.com>
2023-11-27 10:43:01 -0300

    Atribuído faixa de aviso que o paciente retornou em menos de 48 horas

--- Atendimento/views/envio_triagem/lista_paciente_triagem.py
+++ Atendimento/views/envio_triagem/lista_paciente_triagem.py
@@ -38,11 +38,14 @@ def lista_de_paciente_na_triagem(request):
         object_list = envio_triagem.objects.filter(data_envio_triagem = date_hoje) & envio_triagem.objects.exclude(triagem_concluida = 1)
         #object_list = envio_triagem.objects.all()
         #print(f'a data de hoje e {date_hoje}')
-    
+
     pacientes_em_menos_de_48_horas = []
+    
     for triagem in object_list:
         diferenca_tempo = datetime.now() - datetime.combine(triagem.data_envio_triagem, triagem.horario_triagem)
         if diferenca_tempo < timedelta(hours=48):
+            triagem.retornou_em_menos_de_48_horas = True
+            triagem.save()
             pacientes_em_menos_de_48_horas.append(triagem)
 
     data_hoje = datetime.today()    
@@ -51,6 +54,5 @@ def lista_de_paciente_na_triagem(request):
         'lista_db' : object_list,
         'data_hoje' : date_hoje,
         'url' :   'Atendimento:envio_paciente_a_triagem',  
-        'pacientes_em_menos_de_48_horas': pacientes_em_menos_de_48_horas,
     })
 
