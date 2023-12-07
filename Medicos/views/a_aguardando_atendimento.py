dfce40826be4ac9397def229db5bc057c6d4c22e
rogerio <rogerleonino@gmail.com>
2023-12-07 15:16:42 -0300

    resolvendo a duplicação de registros em paciente atendimento

--- Medicos/views/a_aguardando_atendimento.py
+++ Medicos/views/a_aguardando_atendimento.py
@@ -11,11 +11,21 @@ from datetime import datetime
 # Essa View, retorna a lista de pacientes que passaram pela Triagem e aguardam atendimento médico.
 @login_required
 def medico_protuario_view_(request):
-    triagem_filtro = triagem.objects.exclude(final_medico_atendimento = 1)
+    triagem_filtro = triagem.objects.exclude(final_medico_atendimento=1)
+    nao_finalizado = []
+    triage_all = triagem.objects.all()
+
+    for ated_espec in triage_all:
+        try:
+            nao_finalizado.append(ated_espec.medico_atendimento.paciente_medico_atendimento.final_medico_atendimento)
+        except Medico_atendimento.DoesNotExist:
+            pass
+
     return render(request, 'Medicos/medico.html', {
-        'n_pacientes_triagem': len(triagem_filtro),
-        'object_list' : triagem_filtro,
+        'object_list': triagem_filtro,
         'atendimento_especi': Atendimento_especializado.objects.all(),
-        'atendimentos_hoje': Medico_atendimento.objects.filter(data_medico = datetime.now())
+        'atendimentos_hoje': Medico_atendimento.objects.filter(data_medico=datetime.now()),
+        'atend_aguarda_final': triagem_filtro,
+        'nao_finalizado': nao_finalizado
     })
     
