27b85aeba85bbe54fd2d9725fee37301a246f4fa
rogerio <rogerleonino@gmail.com>
2023-12-11 15:10:41 -0300

    exibindo os ultimos chamados

--- Atendimento/views/painel/chamar_paciente.py
+++ Atendimento/views/painel/chamar_paciente.py
@@ -2,22 +2,35 @@ from Medicos.models import Chamar_P_para_atendimento
 from Medicos.models import Chamar_P_para_atendimento
 from django.http import JsonResponse
 from Medicos.models import Chamar_P_para_atendimento, Salas_Atendimento
+from datetime import datetime, timedelta
 from gtts import gTTS
 import io
 import base64
 from django.http import JsonResponse
-from Medicos.models import Chamar_P_para_atendimento, Salas_Atendimento, CadastroSala
+from Medicos.models import Chamar_P_para_atendimento, Salas_Atendimento
+from django.core.serializers import serialize
+from django.utils.safestring import mark_safe
+
 
 
 def chamar_paciente(request):    
     
     # Busca o id do profissional de saúde na primeira chamada de atendimento
-    nome_paciente = Chamar_P_para_atendimento.objects.first().nome_paciente
+    nome_paciente = Chamar_P_para_atendimento.objects.first().nome_paciente    
     profissional_id = Chamar_P_para_atendimento.objects.first().profissionalSaude_id 
     sala_do_usuario = Salas_Atendimento.objects.get(profissionalSaude=profissional_id).nomeSala
 
     nome_paciente_nome = Chamar_P_para_atendimento.objects.first().nome_paciente.paciente_triagem.paciente_envio_triagem.nome_social
     nome_paciente = f'Paciente {nome_paciente}  por favor se dirigir à {sala_do_usuario}'
+
+    data_atual = datetime.now().date()
+
+    # Filtra a queryset para incluir apenas registros do dia atual
+    nome_paciente_chamado = Chamar_P_para_atendimento.objects.filter(data_chamada__date=data_atual)
+    nome_paciente_chamado_data = {}
+    for n in nome_paciente_chamado:
+        nome_paciente_chamado_data = f"<li>Nome: {n.nome_paciente}</li>"
+    #nome_paciente_chamado_data = serialize('json', nome_paciente_chamado)
 
     sala = Salas_Atendimento.objects.filter(profissionalSaude=profissional_id)
     for s in sala:
@@ -35,9 +48,10 @@ def chamar_paciente(request):
     audio_base64 = base64.b64encode(audio_content).decode('utf-8')
 
     # Montar a resposta JSON
-    print(f'nome do paciente {nome_sala}')
+    print(f'nome do paciente {nome_paciente_chamado_data}')
 
     response_data = {
+        'nome_paciente_chamado' : mark_safe(nome_paciente_chamado_data),
         'nome_paciente': nome_paciente_nome,
         'nome_paciente2': nome_paciente_nome,
         'nome_sala': nome_sala,
