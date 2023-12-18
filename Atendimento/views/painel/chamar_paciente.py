21fb9567d55d016d9a808243634e1784ca32abc1
Rogerio da Silva <rogerleonino@gmai.com>
2023-12-18 14:11:09 -0300

    adicionado controle de chamada do paciente

--- Atendimento/views/painel/chamar_paciente.py
+++ Atendimento/views/painel/chamar_paciente.py
@@ -15,42 +15,50 @@ from django.utils.safestring import mark_safe
 
 def chamar_paciente(request):    
     
-    # Busca o id do profissional de saúde na primeira chamada de atendimento
-    nome_paciente = Chamar_P_para_atendimento.objects.filter(chamado=False).first().nome_paciente    
-    profissional_id = Chamar_P_para_atendimento.objects.first().profissionalSaude_id 
-    sala_do_usuario = Salas_Atendimento.objects.get(profissionalSaude=profissional_id).nomeSala
+    try:
+        # Busca o id do profissional de saúde na primeira chamada de atendimento
+        nome_paciente = Chamar_P_para_atendimento.objects.filter(chamado=False).first().nome_paciente    
+        profissional_id = Chamar_P_para_atendimento.objects.first().profissionalSaude_id 
+        sala_do_usuario = Salas_Atendimento.objects.get(profissionalSaude=profissional_id).nomeSala
 
-    nome_paciente_nome = Chamar_P_para_atendimento.objects.first().nome_paciente.paciente_triagem.paciente_envio_triagem.nome_social
-    nome_paciente = f'Paciente {nome_paciente}  por favor se dirigir à {sala_do_usuario}'
-    print(f'nome do paciente {nome_paciente}')
+        nome_paciente_nome = Chamar_P_para_atendimento.objects.first().nome_paciente.paciente_triagem.paciente_envio_triagem.nome_social
+        nome_paciente = f'Paciente {nome_paciente}  por favor se dirigir à {sala_do_usuario}'
+        print(f'nome do paciente {nome_paciente}')
 
-    data_atual = datetime.now().date()
+        data_atual = datetime.now().date()
 
-    # Filtra a queryset para incluir apenas registros do dia atual
-    nome_paciente_chamado = Chamar_P_para_atendimento.objects.filter(data_chamada__date=data_atual)[:5]
-    nome_paciente_chamado_data = {}
-    for n in nome_paciente_chamado:
-        data_chamada = n.data_chamada
-        hora_formato = data_chamada.strftime('%H:%M')
-        nome_paciente_chamado_data = f"<li class='fs-5'><i class='fa-thin fa-hospital-user'></i> {n.nome_paciente} - <i class='fa-regular fa-clock'></i> {hora_formato}</li>"
-    #nome_paciente_chamado_data = serialize('json', nome_paciente_chamado)
+        # Filtra a queryset para incluir apenas registros do dia atual
+        nome_paciente_chamado = Chamar_P_para_atendimento.objects.filter(data_chamada__date=data_atual)[:5]
+        nome_paciente_chamado_data = {}
+        for n in nome_paciente_chamado:
+            data_chamada = n.data_chamada
+            hora_formato = data_chamada.strftime('%H:%M')
+            nome_paciente_chamado_data = f"<li class='fs-5'><i class='fa-thin fa-hospital-user'></i> {n.nome_paciente} - <i class='fa-regular fa-clock'></i> {hora_formato}</li>"
+        #nome_paciente_chamado_data = serialize('json', nome_paciente_chamado)
 
-    sala = Salas_Atendimento.objects.filter(profissionalSaude=profissional_id)
-    for s in sala:
-        nome_sala = s.nomeSala.nome_Sala
-   
+        sala = Salas_Atendimento.objects.filter(profissionalSaude=profissional_id)
+        for s in sala:
+            nome_sala = s.nomeSala.nome_Sala
     
-    # Gerar o áudio em memória
-    tts = gTTS(text=nome_paciente, lang='pt-br')
-    audio_file = io.BytesIO()
-    tts.write_to_fp(audio_file)
-    audio_file.seek(0)
+        
+        # Gerar o áudio em memória
+        tts = gTTS(text=nome_paciente, lang='pt-br')
+        audio_file = io.BytesIO()
+        tts.write_to_fp(audio_file)
+        audio_file.seek(0)
 
-    # Ler os dados do áudio em memória e codificá-los em base64
-    audio_content = audio_file.read()
-    audio_base64 = base64.b64encode(audio_content).decode('utf-8')
+        # Ler os dados do áudio em memória e codificá-los em base64
+        audio_content = audio_file.read()
+        audio_base64 = base64.b64encode(audio_content).decode('utf-8')
 
-    # Montar a resposta JSON
+    except:
+        nome_paciente_chamado_data = ""
+        nome_paciente_nome = ""
+        nome_sala = ""
+
+
+
+   
     
 
     response_data = {
