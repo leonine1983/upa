1842d7ba16078fb0af2b309e2a19f01820b10b87
Rogerio da Silva <rogerleonino@gmail.com>
2024-03-08 11:01:17 -0300

    remoçaõ de comentarios inuteis

--- Medicos/views/a_aguardando_atendimento.py
+++ Medicos/views/a_aguardando_atendimento.py
@@ -8,7 +8,6 @@ from Medicos.models import Medico_atendimento
 from datetime import datetime
 
 
-# Essa View, retorna a lista de pacientes que passaram pela Triagem e aguardam atendimento médico.
 @login_required
 def medico_protuario_view_(request):
     busca_paciente = request.GET.get('busca_paciente')
@@ -16,20 +15,10 @@ def medico_protuario_view_(request):
     if busca_paciente:
         triagem_filtro = triagem.objects.exclude(final_medico_atendimento=1) and triagem.objects.filter(paciente_triagem__paciente_envio_triagem__nome_social__icontains = busca_paciente)    
         checked = 'checked'
-        """
-                   ficha_de_atendimento.objects.filter(cartao_sus__icontains=txt_nome) | \
-                   ficha_de_atendimento.objects.filter(codigo_pacient__icontains=txt_nome) | \
-                   ficha_de_atendimento.objects.filter(CPF__icontains=txt_nome) | \
-                   ficha_de_atendimento.objects.filter(RG__icontains=txt_nome) 
-        
-        
-        """
     else:
         triagem_filtro = triagem.objects.exclude(final_medico_atendimento=1)
         checked = ''
     triage_all = triagem.objects.all()
-
-    
 
     for ated_espec in triage_all:
         
@@ -38,8 +27,6 @@ def medico_protuario_view_(request):
             
         except Medico_atendimento.DoesNotExist:
             pass
-    print(f'esse e o resultado da pesqisa: {nao_finalizado}')
-
     
     return render(request, 'Medicos/medico.html', {
         'object_list': triagem.objects.exclude(final_medico_atendimento=1),
@@ -47,7 +34,6 @@ def medico_protuario_view_(request):
         'atendimentos_hoje': Medico_atendimento.objects.filter(data_medico=datetime.now()),
         'atend_aguarda_final': triagem_filtro,
         'checked5' : checked,
-        'nao_finalizado': nao_finalizado,
-        
+        'nao_finalizado': nao_finalizado,    
     })
     
