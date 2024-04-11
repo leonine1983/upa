05c347218f68df3dd6c0e480f41896741a8f555e
Rogerio da Silva <rogerleonino@gmail.com>
2024-04-11 08:47:48 -0300

    quant por idade

--- Atendimento/views/outras_listagens/exibe_envios_p_data.py
+++ Atendimento/views/outras_listagens/exibe_envios_p_data.py
@@ -31,21 +31,34 @@ class Exibe_envios_data(LoginRequiredMixin, ListView):
             When(paciente_envio_triagem__idade__gte=70, paciente_envio_triagem__idade__lte=79, then=Value('70 a 79 anos')),
             When(paciente_envio_triagem__idade__gte=80, paciente_envio_triagem__idade__lte=110, then=Value('80 a 110 anos')),
             When(paciente_envio_triagem__idade__gte=111, then=Value('Maior que 110')),
-            When(paciente_envio_triagem__idade__isnull=True, then=Value('Idade não informada'))
+            When(paciente_envio_triagem__idade__isnull=True, then=Value('Idade nao informada'))
         ]
         
         # Anotar objetos com a faixa etária correspondente
-        object_list = object_list.annotate(
-            faixa_etaria=Case(*cases, output_field=CharField())
-        )
-        
+        object_list = object_list.annotate(faixa_etaria=Case(*cases, output_field=CharField()))
+
         return object_list
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         
         # Processar a queryset para obter contagens
-        pacientes_por_idade = self.object_list.values('faixa_etaria').annotate(total=Count('id'))
+        contagem_por_faixa_etaria = {
+            '0 a 1 ano': self.object_list.filter(faixa_etaria='0 a 1 ano').count(),
+            '2 a 4 anos': self.object_list.filter(faixa_etaria='2 a 4 anos').count(),
+            '5 a 9 anos': self.object_list.filter(faixa_etaria='5 a 9 anos').count(),
+            '10 a 14 anos': self.object_list.filter(faixa_etaria='10 a 14 anos').count(),
+            '15 a 19 anos': self.object_list.filter(faixa_etaria='15 a 19 anos').count(),
+            '20 a 29 anos': self.object_list.filter(faixa_etaria='20 a 29 anos').count(),
+            '30 a 39 anos': self.object_list.filter(faixa_etaria='30 a 39 anos').count(),
+            '40 a 49 anos': self.object_list.filter(faixa_etaria='40 a 49 anos').count(),
+            '50 a 59 anos': self.object_list.filter(faixa_etaria='50 a 59 anos').count(),
+            '60 a 69 anos': self.object_list.filter(faixa_etaria='60 a 69 anos').count(),
+            '70 a 79 anos': self.object_list.filter(faixa_etaria='70 a 79 anos').count(),
+            '80 a 110 anos': self.object_list.filter(faixa_etaria='80 a 110 anos').count(),
+            'Maior que 110': self.object_list.filter(faixa_etaria='Maior que 110').count(),
+            'Idade nao informada': self.object_list.filter(faixa_etaria='Idade nao informada').count()
+        }
 
         total_paciente_dia = self.object_list.count()
         data_search = self.request.GET.get('busca_data', datetime.today().strftime('%Y-%m-%d'))
@@ -54,6 +67,6 @@ class Exibe_envios_data(LoginRequiredMixin, ListView):
         # Adicionar as contagens ao contexto
         context['data'] = data
         context['total_paciente'] = total_paciente_dia 
-        context['pacientes_por_idade'] = pacientes_por_idade
+        context['contagem_por_faixa_etaria'] = contagem_por_faixa_etaria
 
         return context
