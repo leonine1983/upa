40771b858cccd4214241b18f0ef050c005cd0039
Rogerio da Silva <rogerleonino@gmail.com>
2024-04-22 14:20:21 -0300

    resolvido o erro de date

--- Atendimento/views/outras_listagens/exibe_envios_p_data.py
+++ Atendimento/views/outras_listagens/exibe_envios_p_data.py
@@ -3,8 +3,10 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from Atendimento.models import envio_triagem, genero_sexual, Bairro, Cidade
 from django.views.generic.list import ListView
 from datetime import datetime
-from django.db.models import Count, Case, When, CharField, Value
+from django.db.models import Case, When, CharField, Value
 from collections import defaultdict
+from django.urls import reverse, reverse_lazy
+from django.http import HttpResponseRedirect
 
 class Exibe_envios_data(LoginRequiredMixin, ListView):
     model = envio_triagem
@@ -12,36 +14,38 @@ class Exibe_envios_data(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         # Data de início fornecida pela consulta GET, se não fornecida, use a data de hoje
-        start_date = self.request.GET.get('busca_data', datetime.today().strftime('%Y-%m-%d'))
-        
-        # Filtrar objetos pelo data_envio_triagem
-        object_list = envio_triagem.objects.filter(data_envio_triagem=start_date)
-        self.envios_day = object_list
-        
-        # Casos para calcular faixa etária
-        cases = [
-            When(paciente_envio_triagem__idade__gte=0, paciente_envio_triagem__idade__lte=1, then=Value('0 a 1 ano')),
-            When(paciente_envio_triagem__idade__gte=2, paciente_envio_triagem__idade__lte=4, then=Value('2 a 4 anos')),
-            When(paciente_envio_triagem__idade__gte=5, paciente_envio_triagem__idade__lte=9, then=Value('5 a 9 anos')),
-            When(paciente_envio_triagem__idade__gte=10, paciente_envio_triagem__idade__lte=14, then=Value('10 a 14 anos')),
-            When(paciente_envio_triagem__idade__gte=15, paciente_envio_triagem__idade__lte=19, then=Value('15 a 19 anos')),
-            When(paciente_envio_triagem__idade__gte=20, paciente_envio_triagem__idade__lte=29, then=Value('20 a 29 anos')),
-            When(paciente_envio_triagem__idade__gte=30, paciente_envio_triagem__idade__lte=39, then=Value('30 a 39 anos')),
-            When(paciente_envio_triagem__idade__gte=40, paciente_envio_triagem__idade__lte=49, then=Value('40 a 49 anos')),
-            When(paciente_envio_triagem__idade__gte=50, paciente_envio_triagem__idade__lte=59, then=Value('50 a 59 anos')),
-            When(paciente_envio_triagem__idade__gte=60, paciente_envio_triagem__idade__lte=69, then=Value('60 a 69 anos')),
-            When(paciente_envio_triagem__idade__gte=70, paciente_envio_triagem__idade__lte=79, then=Value('70 a 79 anos')),
-            When(paciente_envio_triagem__idade__gte=80, paciente_envio_triagem__idade__lte=110, then=Value('80 a 110 anos')),
-            When(paciente_envio_triagem__idade__gte=111, then=Value('Maior que 110')),
-            When(paciente_envio_triagem__idade__isnull=True, then=Value('Idade nao informada'))
-        ]
+        try:
+            start_date = self.request.GET.get('busca_data', datetime.today().strftime('%Y-%m-%d'))    
+            # Filtrar objetos pelo data_envio_triagem
+            object_list = envio_triagem.objects.filter(data_envio_triagem=start_date)
+            self.envios_day = object_list
+            
+            # Casos para calcular faixa etária
+            cases = [
+                When(paciente_envio_triagem__idade__gte=0, paciente_envio_triagem__idade__lte=1, then=Value('0 a 1 ano')),
+                When(paciente_envio_triagem__idade__gte=2, paciente_envio_triagem__idade__lte=4, then=Value('2 a 4 anos')),
+                When(paciente_envio_triagem__idade__gte=5, paciente_envio_triagem__idade__lte=9, then=Value('5 a 9 anos')),
+                When(paciente_envio_triagem__idade__gte=10, paciente_envio_triagem__idade__lte=14, then=Value('10 a 14 anos')),
+                When(paciente_envio_triagem__idade__gte=15, paciente_envio_triagem__idade__lte=19, then=Value('15 a 19 anos')),
+                When(paciente_envio_triagem__idade__gte=20, paciente_envio_triagem__idade__lte=29, then=Value('20 a 29 anos')),
+                When(paciente_envio_triagem__idade__gte=30, paciente_envio_triagem__idade__lte=39, then=Value('30 a 39 anos')),
+                When(paciente_envio_triagem__idade__gte=40, paciente_envio_triagem__idade__lte=49, then=Value('40 a 49 anos')),
+                When(paciente_envio_triagem__idade__gte=50, paciente_envio_triagem__idade__lte=59, then=Value('50 a 59 anos')),
+                When(paciente_envio_triagem__idade__gte=60, paciente_envio_triagem__idade__lte=69, then=Value('60 a 69 anos')),
+                When(paciente_envio_triagem__idade__gte=70, paciente_envio_triagem__idade__lte=79, then=Value('70 a 79 anos')),
+                When(paciente_envio_triagem__idade__gte=80, paciente_envio_triagem__idade__lte=110, then=Value('80 a 110 anos')),
+                When(paciente_envio_triagem__idade__gte=111, then=Value('Maior que 110')),
+                When(paciente_envio_triagem__idade__isnull=True, then=Value('Idade nao informada'))
+            ]        
+            
+            # Anotar objetos com a faixa etária correspondente
+            object_list = object_list.annotate(faixa_etaria=Case(*cases, output_field=CharField()))
+            return object_list
+            
+        except Exception as e:
+            return reverse_lazy('Atendimento:listagem_envio')          
 
-       
         
-        # Anotar objetos com a faixa etária correspondente
-        object_list = object_list.annotate(faixa_etaria=Case(*cases, output_field=CharField()))
-
-        return object_list
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -62,9 +66,7 @@ class Exibe_envios_data(LoginRequiredMixin, ListView):
             '80 a 110': self.object_list.filter(faixa_etaria='80 a 110 anos').count(),
             'Maior que 110': self.object_list.filter(faixa_etaria='Maior que 110').count(),
             'Idade nao informada': self.object_list.filter(faixa_etaria='Idade nao informada').count()
-        }
-
-        
+        }       
         
         envios_por_genero = defaultdict(dict)
         envio_por_bairro = defaultdict(dict)
